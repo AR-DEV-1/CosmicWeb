806f77ba71cc5638fce208dafd72251ce6f37386
AR-DEV-1 <mail.ardev1@protonmail.com>
2024-08-29 16:14:55 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#define COSMIC_WEB_ENGINE
+#define COSMIC_WEB_ENGINE_IMPLEMENTATION
+#include "include/engine.h"
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "include/stb_truetype.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+// Copyright AR-DEV-1
+// Copyright The Cosmic Web Authors
+// This software and its source is provided under GNU GPL 3.0
+// The runtime, software, source and etc are all provided under GNU GPL 3.0
