96ff6fe893b77827b2cdcce8203da893cddaf0b8
AR-DEV-1 <mail.ardev1@protonmail.com>
2024-08-30 15:54:23 +0500

    Update engine.h

--- include/engine.h
+++ include/engine.h
@@ -4,22 +4,4 @@
 // Copyright The Cosmic Web Authors
 // This software and its source is provided under GNU GPL 3.0
 // The runtime, software, source and etc are all provided under and GNU GPL 3.0
-#define SYSTEM
-#define COSMIC_WEB_ENGINE
-#define COSMIC_WEB_ENGINE_IMPLEMENTATION
-#define ENGINE_HELPER
-#define ENGINE_CALL_API
-#define COSMIC_WEB_API_IMPLEMENTATION
-#define COSMIC_WEB_API_HELPER
-#define HTML_CSS_JS
-#define HTTPS_HTTP_FTP
 
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include <cstdlib>
-#include <stdlib.h>
-
-
-void startEngine()
-void callAPI()
