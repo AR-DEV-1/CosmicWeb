34194d91593cc93cf7a5d17bc94209baf54bc429
AR-DEV-1 <mail.ardev1@protonmail.com>
2024-09-25 14:57:11 +0500

    Use Symm for Graphics and Audio except for Gl and that stuff.
    https://github.com/AR-DEV-1/Symm

---
+++ include/Symm/symm.h
@@ -0,0 +1,32 @@
+#ifndef SYMM_H
+#define SYMM_H
+
+#ifdef SYMM_IMPLEMENTATION
+#define SYMM_API extern "C"
+#else
+#define SYMM_API
+#endif
+
+// Define the graphics module
+#ifdef SYMM_GRAPHICS_IMPLEMENTATION
+namespace symm_graphics {
+    SYMM_API void InitGraphics(); // Init Graphics
+    SYMM_API void RenderFrame(); // Render a frame
+    SYMM_API void createObject();  // Create a object
+    SYMM_API void renderShaders(); // Render shaders
+    SYMM_API void SymmCreateShaders(); // Create shaders
+    SYMM_API void renderFont(); //Render font
+}
+#endif
+
+// Define the audio module
+#ifdef SYMM_AUDIO_IMPLEMENTATION
+namespace symm_audio {
+    SYMM_API void InitAudio(); // Init Audio
+    SYMM_API void PlaySound(); // Play sound/s
+    SYMM_API void replayAudio(); // Replay audio
+    SYMM_API void AudioTimestamp(); // The audio timestamp
+}
+#endif
+
+#endif // SYMM_H
